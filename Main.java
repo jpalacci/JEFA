9c171c4b3f7d8f3706ad45ceebcce434f0b6b735
Francisco Sammartino <frperez@itba.edu.ar>
2018-06-28 10:24:16 -0300

    cambios de ayer

--- Main.java
+++
@@ -1,7 +0,0 @@
-import dk.brics.automaton.*;import java.io.PrintWriter;import java.util.Scanner; public class Main { public static void main(String[] args) {Scanner scan = new Scanner(System.in);
-PrintWriter graph;
-Automaton a=new RegExp("a+b+").toAutomaton();
-Automaton b=a.complement().complement();
-System.out.println(a.equals(b));
-scan.close();
-}}
